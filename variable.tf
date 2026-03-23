a625f589e95d59fd5e05ff699552430a27dc4327
rbrgmn <152183759+rbrgmn@users.noreply.github.com>
2026-03-23 12:35:42 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "proxmox_api_url" {
+  type        = string
+  description = "Proxmox API url"
+}
+
+variable "proxmox_api_token_id" {
+  type = string
+}
+
+variable "proxmox_api_token_secret" {
+  type      = string
+  sensitive = true
+}
+
