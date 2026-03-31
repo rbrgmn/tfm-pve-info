52a1652fee49f62f23cfcfdd9f37eb2471836a85
rbrgmn <152183759+rbrgmn@users.noreply.github.com>
2026-03-31 20:57:24 +0200

    Update variables_vms_templates.tf

--- variables_vms_templates.tf
+++ variables_vms_templates.tf
@@ -3,7 +3,7 @@ variable "pve_vm_import" {
   type = map(string)
   default = {
     rocky9 = "images:import/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2"
-    rocky10 = "images:import/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2"
+    rocky10 = "images:import/Rocky-10-GenericCloud-Base.latest.x86_64.qcow2"
     alma9 = "images:import/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2"
     alma10 = "images:import/AlmaLinux-10-GenericCloud-latest.x86_64.qcow2"
   }
